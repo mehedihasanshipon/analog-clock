0952eba663f37a280cff89c7622d08e12f95074c
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-02-04 18:20:27 +0600

    Alalog clock using HTML,CSS and pure JS

---
+++ script.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    let date = new Date();
+    let hourTime = date.getHours();
+    let minuteTime = date.getMinutes();
+    let secondTime = date.getSeconds();
+    // console.log(secondTime);
+    let hourRotation = 30*hourTime + minuteTime/2;
+    let minuteRotation = 6 * minuteTime;
+    let secondRotation = 6*secondTime;
+
+    hour.style.transform = `rotate(${hourRotation}deg)`;
+    minute.style.transform = `rotate(${minuteRotation}deg)`;
+    second.style.transform = `rotate(${secondRotation}deg)`;
+},1000)
